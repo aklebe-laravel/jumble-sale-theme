416c039717bfb9284bb5458debe314c0a920a57c
Andreas Klebe <a.klebe.work@gmail.com>
2025-01-24 10:42:02 +0100

    - fixed image uploads  - for nom media item always a new media item will be created instead to change the image - Import status results - adjusted Form descriptions - fixed product form is_test default

--- views/notifications/telegram/market-media-item-import.blade.php
+++ views/notifications/telegram/market-media-item-import.blade.php
@@ -1,4 +1,4 @@
 {{--telegram HTML mode supports: <b></b>, <i></i>, <s></s>, <u></u>--}}
 <b>{{ __('Hello :name', ['name' => $user->name]) }}</b>
 
-{{ __('Your import was completed. :current/:total rows were imported.', ['current' => 0, 'total' => 0]) }}
+{{ __('Your import was completed. :current/:total rows were imported.', ['current' => $results->getData('import_rows_success' , '?'), 'total' => $results->getData('import_rows_requested' , '?')]) }}
